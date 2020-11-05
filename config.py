df90b7d69331699cc5f20862ffb00d2f46b7187d
stasandrey <staschenyuk.andrey@gmail.com>
2020-11-05 12:57:47 +0300

    Добавлены config.py и log.py

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
+
+import configparser
+
+class Configuration:
+    DEFAULT = { 'Logging':{ 'LogToFile':'False'
+                            'LogFileName':'znak_def.log', 
+                            'LogToConsole':'True', 
+                            'LogLevel':'INFO', 
+                            'AutoOpenConsoleWindow':'True'
+                            }
+        
+        
+                }
+    
+    def __init__( self, name ):
+        self.cfg = configparser.ConfigParser()
+        self.cfg.read( name )
+    
+    def get( self, section, item ):
+        res = None
+        if section in self.cfg:
+            if item in self.cfg[section]:
+                res = self.cfg[section][item]
+        if res == None:
+            res = self.DEFAULT[section][item]
+        return res
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- codingtype=utf-8-*-
-import logging
 
+import config
 
-
+CONFIG_FILE_NAME = "znak.ini"
 
 if __name__ == "__main__":
-    print( "hello" )
-    
+    cfg = config.Configuration( CONFIG_FILE_NAME )
+    print( cfg.get( 'Logging', 'LogLevel' ) )
