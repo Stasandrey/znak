b87287e80406f3c60c58c454f5202f219aed1271
stasandrey <staschenyuk.andrey@gmail.com>
2020-11-18 13:44:51 +0300

    Формируются этикетки по НС, без занесения в базу данных.

--- main_window.py
+++ main_window.py
@@ -3,77 +3,80 @@
 
 from PyQt5 import QtCore, QtWidgets
 import random
-#import options_window
+# import options_window
 import console_window
 import otgruzka_window
 import scan_codes_window
 
 import Ui_MainWindow
 
-class MainWindow( QtWidgets.QMainWindow, Ui_MainWindow.Ui_MainWindow ):
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow.Ui_MainWindow):
     console = None
     isConsole = False
-    def __init__( self, log, cfg, db, api, parent = None):
+
+    def __init__(self, log, cfg, db, api, parent=None):
         self.log = log
         self.cfg = cfg
         self.db = db
         self.api = api
-        self.log.info( "Конструктор главного окна" )
-        QtWidgets.QWidget.__init__( self, parent )
-        self.setupUi( self )
-        self.setCentralWidget( self.mdiArea )
-        self.log.info( "Создание окна консоли" )  
-        self.console = console_window.ConsoleWindow( log, cfg )
-        self.console.setAttribute( QtCore.Qt.WA_DeleteOnClose )
+        self.log.info("Конструктор главного окна")
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setupUi(self)
+        self.setCentralWidget(self.mdiArea)
+        self.log.info("Создание окна консоли")
+        self.console = console_window.ConsoleWindow(log, cfg)
+        self.console.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.log.consoleWindow = self.console
-        if self.cfg.get( 'Logging', 'AutoOpenConsoleWindow' ) == 'True':
+        if self.cfg.get('Logging', 'AutoOpenConsoleWindow') == 'True':
             self.isConsole = True
-            self.mdiArea.addSubWindow( self.console )
+            self.mdiArea.addSubWindow(self.console)
             self.console.show()
-        self.actCreateDatabase.triggered.connect( self.doCreateDatabase )
-        self.actChangeDatabase.triggered.connect( self.doChangeDatabase )
-        self.actSyncDatabase.triggered.connect( self.doSyncDatabase )
-        self.actConsole.triggered.connect( self.doConsole )
-        self.actOptions.triggered.connect( self.doOptions )
-        self.actExit.triggered.connect( self.close )
-        self.actNewOtgruzka.triggered.connect( self.doNewOtgruzka )
-        self.actChangeOtgruzka.triggered.connect( self.doChangeOtgruzka )
-        self.actScanCodes.triggered.connect( self.doScanCodes )
-    
-    def doScanCodes( self ):
-        self.scanCodes = scan_codes_window.ScanCodesWindow( self.log, self.cfg,
-                                                       self.db, self.api )
-        self.scanCodes.setAttribute( QtCore.Qt.WA_DeleteOnClose )
-        self.mdiArea.addSubWindow( self.scanCodes )
-        self.scanCodes.resize( 400, 400 )
+        self.actCreateDatabase.triggered.connect(self.doCreateDatabase)
+        self.actChangeDatabase.triggered.connect(self.doChangeDatabase)
+        self.actSyncDatabase.triggered.connect(self.doSyncDatabase)
+        self.actConsole.triggered.connect(self.doConsole)
+        self.actOptions.triggered.connect(self.doOptions)
+        self.actExit.triggered.connect(self.close)
+        self.actNewOtgruzka.triggered.connect(self.doNewOtgruzka)
+        self.actChangeOtgruzka.triggered.connect(self.doChangeOtgruzka)
+        self.actScanCodes.triggered.connect(self.doScanCodes)
+
+    def doScanCodes(self):
+        self.scanCodes = scan_codes_window.ScanCodesWindow(self.log, self.cfg,
+                                                           self.db, self.api)
+        self.scanCodes.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.mdiArea.addSubWindow(self.scanCodes)
+        self.scanCodes.resize(400, 400)
         self.scanCodes.show()
-    
-    def doNewOtgruzka( self ):
-        s, ok = QtWidgets.QInputDialog.getText( self, "Новая отгрузка", 
-                                            "Введите название отгрузки" )
+
+    def doNewOtgruzka(self):
+        s, ok = QtWidgets.QInputDialog.getText(self, "Новая отгрузка",
+                                               "Введите название отгрузки")
         if ok:
-            tblName = "TBL_" + str( random.randrange( 1000000, 9999999 ) )
-            while len( self.db.runSql( "SELECT * FROM OTGRUZKI WHERE TBL_NAME = '%s';"%(
-                          tblName ) ) ) > 0:
-                tblName = str( random.randrange( 1000000, 9999999 ) )
-            self.db.runSql( "INSERT INTO OTGRUZKI VALUES ( '%s', '%s' );"%(
-                          s, tblName) )
-            self.db.runSql( "CREATE TABLE %s ( MODEL TEXT, SIZE TEXT, GTIN TEXT, SOURCE TEXT, NUMBER INTEGER );"%( tblName ) )
-            print( "CREATE TABLE %s ( GTIN TEXT, SOURCE TEXT, NUMBER INTEGER );"%( tblName ) )
-            self.log.info( 'Создание отгрузки %s TBL_NAME = %s'%( s, tblName ) )
+            tblName = "TBL_" + str(random.randrange(1000000, 9999999))
+            while len(self.db.runSql("SELECT * FROM OTGRUZKI WHERE TBL_NAME = '%s';" % (
+                    tblName))) > 0:
+                tblName = str(random.randrange(1000000, 9999999))
+            self.db.runSql("INSERT INTO OTGRUZKI VALUES ( '%s', '%s' );" % (
+                s, tblName))
+            self.db.runSql(
+                "CREATE TABLE %s ( MODEL TEXT, SIZE TEXT, GTIN TEXT, SOURCE TEXT, NUMBER INTEGER );" % (tblName))
+            print("CREATE TABLE %s ( GTIN TEXT, SOURCE TEXT, NUMBER INTEGER );" % (tblName))
+            self.log.info('Создание отгрузки %s TBL_NAME = %s' % (s, tblName))
             self.db.otgruska = tblName
-            self.doChangeOtgruzka( tblName )
-    
-    def doChangeOtgruzka( self, name = '' ):
-        list = [] 
-        res = self.db.runSql( "SELECT * FROM OTGRUZKI;" )
+            self.doChangeOtgruzka(tblName)
+
+    def doChangeOtgruzka(self, name=''):
+        list = []
+        res = self.db.runSql("SELECT * FROM OTGRUZKI;")
         for item in res:
-            list.append( item['NAME'] )
+            list.append(item['NAME'])
         if name == False:
-            s, ok = QtWidgets.QInputDialog.getItem( self, "Смена отгрузки", 
-                                                    "Выберите отгрузку", 
-                                                    list, 
-                                                    current = 0)
+            s, ok = QtWidgets.QInputDialog.getItem(self, "Смена отгрузки",
+                                                   "Выберите отгрузку",
+                                                   list,
+                                                   current=0)
             if ok:
                 name = s
         if name != '':
@@ -81,69 +84,70 @@ class MainWindow( QtWidgets.QMainWindow, Ui_MainWindow.Ui_MainWindow ):
                 for i in res:
                     if name == i['NAME']:
                         tbl = i['TABLE_NAME']
-                self.otgruzka = otgruzka_window.OtgruzkaWindow( self.log, self.cfg,
-                                                                self.db, self.api, 
-                                                                tbl, name )
-                self.otgruzka.setAttribute( QtCore.Qt.WA_DeleteOnClose )
-                self.mdiArea.addSubWindow( self.otgruzka )
-                self.otgruzka.resize( 400, 400 )
+                self.otgruzka = otgruzka_window.OtgruzkaWindow(self.log, self.cfg,
+                                                               self.db, self.api,
+                                                               tbl, name)
+                self.otgruzka.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+                self.mdiArea.addSubWindow(self.otgruzka)
+                self.otgruzka.resize(400, 400)
                 self.otgruzka.show()
-    
-    def doCreateDatabase( self ):
-        res = QtWidgets.QFileDialog.getSaveFileName( caption = "Выберите имя и место хранения базы данных.", 
-                               directory = QtCore.QDir.currentPath(), 
-                               filter = "All (*)")
+
+    def doCreateDatabase(self):
+        res = QtWidgets.QFileDialog.getSaveFileName(caption="Выберите имя и место хранения базы данных.",
+                                                    directory=QtCore.QDir.currentPath(),
+                                                    filter="All (*)")
         if res[0] != "":
-            self.log.info( "Создание новой базы данных%s"%( res[0] ) )
-            self.db.createDatabase( res[0] )
-        
-    def doChangeDatabase( self ):
+            self.log.info("Создание новой базы данных%s" % (res[0]))
+            self.db.createDatabase(res[0])
+
+    def doChangeDatabase(self):
         pass
 
-    def doSyncDatabase( self ):
-        self.log.info( "Синхронизация базы данных с системой Честный знак" )
-        res = ( self.api.do( 'get_gtins', {} ) )['data']['results']
+    def doSyncDatabase(self):
+        self.log.info("Синхронизация базы данных с системой Честный знак")
+        res = (self.api.do('get_gtins', {}))['data']['results']
         n = 0
         gtin = []
         for i in res:
-            mr = ['','']
+            mr = ['', '']
             if 'model' in i:
                 if 'productSize' in i:
-                    if 'gtin' in i:    
+                    if 'gtin' in i:
                         mr = []
-                        mr.append( i['gtin'] )
-                        mr.append( i['model'][i['model'].find( 'Модель' ) + 7:len( i['model'] )] )
-                        mr.append( i['productSize'] )
-                        gtin.append( mr )
+                        mr.append(i['gtin'])
+                        mr.append(i['model'][i['model'].find('Модель') + 7:len(i['model'])])
+                        mr.append(i['productSize'])
+                        gtin.append(mr)
                         n = n + 1
-        self.log.info( 'Получено %s кодов.'%( n ) )
-        self.log.info( "Удаление записей" )
-        self.db.runSql( "DELETE FROM GTINS;" )
-        self.log.info( "Запись в базуданных" )
+        self.log.info('Получено %s кодов.' % (n))
+        self.log.info("Удаление записей")
+        self.db.runSql("DELETE FROM GTINS;")
+        self.log.info("Запись в базуданных")
         n = 0
         for item in gtin:
-            self.db.runSql( "INSERT INTO GTINS VALUES ( '%s', '%s', '%s' );"%(
-                                item[0], item[1], item[2] ) )
+            self.db.runSql("INSERT INTO GTINS VALUES ( '%s', '%s', '%s' );" % (
+                item[0], item[1], item[2]))
             n = n + 1
-            print( n )
-        self.log.info( 'OK' )
+            print(n)
+        self.log.info('OK')
 
-    def doConsole( self ):
-        self.log.info( "Показать консоль" )
-        if self.isConsole ==False:
+    def doConsole(self):
+        self.log.info("Показать консоль")
+        if self.isConsole == False:
             self.isConsole = True
-            self.mdiArea.addSubWindow( self.console )
+            self.mdiArea.addSubWindow(self.console)
             self.console.show()
         else:
             self.isConsole = False
             self.console.hide()
-            self.mdiArea.removeSubWindow( self.console )
- 
-    def doOptions( self ):
-        self.log.warning( "Показать Настройки" )
-        #options = options_window.OptionsWindow()
-        #options.exec()
+            self.mdiArea.removeSubWindow(self.console)
+
+    def doOptions(self):
+        self.log.warning("Показать Настройки")
+        # options = options_window.OptionsWindow()
+        # options.exec()
+
 
 if __name__ == "__main__":
-    print( "Этот модуль является частью приложения." )
-    print( "Для запуска приложения выполните main.py" )
+    print("Этот модуль является частью приложения.")
+    print("Для запуска приложения выполните main.py")
--- scan_codes_window.py
+++ scan_codes_window.py
@@ -1,6 +1,11 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
 import fitz
+import datetime
+from barcode import EAN13
+from barcode.writer import  ImageWriter
+from wand.image import Image
+from wand.drawing import Drawing
 
 from PyQt5 import QtWidgets, QtCore
 import Ui_ScanCodesWindow
@@ -30,9 +35,73 @@ class ScanCodesWindow( QtWidgets.QMainWindow, Ui_ScanCodesWindow.Ui_ScanCodesWin
                 
     def scanNS( self, name ):
         doc = fitz.open( name )
+        text = ''
         for page in doc:
-            print( page.getImageList( full = True ) )
-            print( page.getText() )
+            # print( page.getImageList( full = True ) )
+            text = text + page.getText()
+        items = text.split( '\n' )
+        flag = 0
+        s = ''
+        table = []
+        for item in items:
+            if flag == 1:
+                s = s + item
+                flag = 0
+                table.append( s )
+            if flag == 0:
+                if item[0:4] == '0104':
+                    s = item
+                    flag = 1
+        data = []
+        for item in table:
+            gtin = item[2:16]
+            time = str( datetime.datetime.now() )
+            filename = ''
+            for q in time:
+                if q == ':' or q == '-' or q == ' ' or q == '.':
+                    filename = filename + '_'
+                else:
+                    filename = filename + q
+            filename = gtin + '_' + filename
+            code = { 'gtin':gtin, 'code':item, 'filename':filename }
+            data.append( code )
+        for item in data:
+            print( item )
+        for i, item in enumerate( data ):
+            self.readImageNs( name, i, "images/%s"%( data[i]['filename'] ), data[i]['gtin'] )
+# --------------------------------------------------------------------------------------------
+    def readImageNs( self, name, i, filename, gtin ):
+        koord = [[0, 415],
+                 [420, 835],
+                 [840, 1255],
+                 [1260, 1675],
+                 [1680, 2095],
+                 [2105, 2520],
+                 [2525, 2940],
+                 [2945, 3360]]
+        page = int(i / 8)
+        n = i % 8
+        print("Page %s" % (page))
+        print("Image %s" % (n))
+
+        print(gtin[1:14])
+        with open('ean.png', 'wb') as f:
+            EAN13('%s' % (gtin[1:13]), writer=ImageWriter()).write(f)
+        with  Image(filename='%s[%s]' % (name, page), resolution=300) as img:
+            img.convert('png')
+            img.crop(0, koord[n][0], 709, koord[n][1])
+            draw = Drawing()
+            ean = Image(filename="./ean.png", resolution=300).clone()
+            draw.composite(operator="over", left=5, top=235, width=round(ean.width * 0.65),
+                           height=round(ean.height * 0.65), image=ean)
+            draw(img)
+            img.save(filename='%s.png' % (filename))
+
+
+# --------------------------------------------------------------------------------------------
+
+    def scanDS( self, item ):
+        pass
 
     def doChooseFiles( self ):
         res = QtWidgets.QFileDialog.getOpenFileNames( parent = self, caption = "Выбор файлов", 
